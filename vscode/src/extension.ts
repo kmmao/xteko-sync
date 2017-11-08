276cb9347933e8afc178e933452ac6b0a9da618f
cyan <0x00eeee@gmail.com>
2017-11-08 17:36:55 +0800

    Code convention

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -22,7 +22,7 @@ export function activate(context: ExtensionContext) {
     let path = window.activeTextEditor.document.fileName;
     let watcher = workspace.createFileSystemWatcher(path);
 
-    watcher.onDidChange((event) => {
+    watcher.onDidChange(event => {
 
         // Check host is available
         let host = workspace.getConfiguration().host;
@@ -36,7 +36,7 @@ export function activate(context: ExtensionContext) {
         var request = require('request');
         var fs = require('fs');
         var formData = {'files[]': fs.createReadStream(path)};
-        request.post({url: 'http://' + host + '/upload', formData: formData}, (error) => {
+        request.post({url: 'http://' + host + '/upload', formData: formData}, error => {
             if (error) {
                 onError(error);
             }
